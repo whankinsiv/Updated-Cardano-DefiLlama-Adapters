0f4bc3dcaf380d8234510f0bd0386cf3625a965d
Will Hankins <will@tidelabs.io>
2025-04-01 14:11:51 -0700

    Simplified SundaeSwap V3 Adapter

--- sundaeswap-v3/index.js
+++ sundaeswap-v3/index.js
@@ -1,28 +1,12 @@
-const { getAssets } = require('../helper/chain/cardano/blockfrost')
+// SundaeSwap V3
+const { sumTokens2 } = require('../helper/chain/cardano');
+const ammLockedAssets = ["addr1x8srqftqemf0mjlukfszd97ljuxdp44r372txfcr75wrz26rnxqnmtv3hdu2t6chcfhl2zzjh36a87nmd6dwsu3jenqsslnz7e", "addr1z8srqftqemf0mjlukfszd97ljuxdp44r372txfcr75wrz2auzrlrz2kdd83wzt9u9n9qt2swgvhrmmn96k55nq6yuj4qw992w9"];
 
 async function tvl() {
-    const [ammLockedAssets, ammLockedAssets2] = await Promise.all([
-        getAssets("addr1x8srqftqemf0mjlukfszd97ljuxdp44r372txfcr75wrz26rnxqnmtv3hdu2t6chcfhl2zzjh36a87nmd6dwsu3jenqsslnz7e"),
-        getAssets("addr1z8srqftqemf0mjlukfszd97ljuxdp44r372txfcr75wrz2auzrlrz2kdd83wzt9u9n9qt2swgvhrmmn96k55nq6yuj4qw992w9"),
-    ]);
-
-    // Merge the two arrays and sum quantities for duplicate units
-    const combinedAssets = [...ammLockedAssets, ...ammLockedAssets2].reduce((acc, asset) => {
-        const existingAsset = acc.find(a => a.unit === asset.unit);
-        if (existingAsset) {
-            existingAsset.quantity = (Number(existingAsset.quantity) + Number(asset.quantity)).toString();
-        } else {
-            acc.push({ ...asset });
-        }
-        return acc;
-    }, []);
-
-    const formattedData = combinedAssets.reduce((result, item) => {
-        result[`cardano:${item.unit}`] = item.quantity;
-        return result;
-    }, {});
-
-    return formattedData
+    const lockedAssets = await sumTokens2({
+        owners: ammLockedAssets
+    })
+    return lockedAssets
 }
 
 module.exports = {
